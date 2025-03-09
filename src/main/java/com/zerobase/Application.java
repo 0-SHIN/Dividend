022d8815d0fff3b3159ba67bffb37adc88b85c91
YoungshinHong <hys3448@gmail.com>
2025-03-09 15:39:27 +0900

    수정 사항

--- src/main/java/com/zerobase/Application.java
+++ src/main/java/com/zerobase/Application.java
@@ -2,14 +2,16 @@ package com.zerobase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableCaching
 @EnableScheduling
 @SpringBootApplication
-public class SampleApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SampleApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
